75f5b7d8dc645269d0ebd8f707b5f8f8838029e9
Nelida Valero <nelidavalero06@gmail.com>
2021-05-10 11:08:28 -0500

    cambios en publicaciones JS y CSS

--- src/lib/publicaciones.js
+++ src/lib/publicaciones.js
@@ -3,26 +3,23 @@ export const publicaciones = () => {
   contenedorPublicaciones.className = 'contenedorPublicaciones';
 
   const contenidoEncabezadopublicaciones = `
-  <img alt="logoPublicaciones" class="logoPublicar" id="logoPublicaciones"src = "Imagenes/LogoBeer.png"/>
-  
-  <div class = "iconosEnlaces">
-  <img alt="perfil" class="iconoPerfil" id="perfilUsuario"src = "Imagenes/hombre.png"/>
-  <img alt="busqueda" class="iconoBuscar" id="buscar"src = "Imagenes/lupa.png"/>
-  <img alt="home" class="iconoHome" id="home"src = "Imagenes/casa.png"/>
-  <img alt="brindis" class="iconoLikes" id="likes"src = "Imagenes/salud.png"/>
-  <img alt="salida" class="iconoSalir" id="salir"src = "Imagenes/salida.png"/>
-  </div>
-  <div id="listadoPublicaciones">
-  <img alt="brindis" class="iconoLikes" id="likes"src = "Imagenes/salud.png"/>
-  </div>
-
-
-  <footer class="footerPublicaciones">
-  
-  <img alt="newPost" class="iconoPublicar" id="publicar"src = "Imagenes/mas.png"/>
-  </footer>
-      `;
+    <div class="contenedorLogoPublicaciones">
+    <img alt="logoPublicaciones" class="logoPublicar" id="logoPublicaciones"src = "Imagenes/LogoBeer.png"/>
+    </div>
+    <div class= "iconosEnlaces">
+      <img alt="perfil" class="iconosNavegacion" id="perfilUsuario"src = "Imagenes/hombre.png"/>
+      <img alt="busqueda" class="iconosNavegacion" id="buscar"src = "Imagenes/lupa.png"/>
+      <img alt="home" class="iconosNavegacion" id="home"src = "Imagenes/casa.png"/>
+      <img alt="brindis" class="iconosNavegacion" id="likes"src = "Imagenes/salud.png"/>
+      <img alt="salida" class="iconosNavegacion" id="salir"src = "Imagenes/salida.png"/>
+    </div>
+    <div class="footerPublicaciones">
+     <img alt="newPost" class="iconoPublicar" id="publicar"src = "Imagenes/mas.png"/>
+    </div>
+  `;
   contenedorPublicaciones.innerHTML = contenidoEncabezadopublicaciones;
-
   return contenedorPublicaciones;
 };
+{/* <div id="listadoPublicaciones">
+      <img alt="brindis" class="iconoLikes" id="likes" src = "Imagenes/salud.png"/>
+</div> */}
