c64de4273e3b0d7560f5b92dd6caa87af61fae8e
JessiJoya <jessica.joyaq@gmail.com>
2021-05-24 18:30:08 -0500

    cambia el nombre de los botones pero abre de nuevo el post

--- src/views.js
+++ src/views.js
@@ -1,7 +1,7 @@
 import { pantallaInicio } from './lib/inicio.js';
 import { registroUsuario } from './lib/registroUsuario.js';
 import { inicioSesion } from './lib/inicioSesion.js';
-import { publicaciones, viewPost} from './lib/publicaciones.js';
+import { publicaciones, viewPost } from './lib/publicaciones.js';
 import { registerUSer, loginUSer, registroGmail } from './firebaseAuth.js';
 
 const data = firebase.firestore();
@@ -42,18 +42,27 @@ const onGetPost = (callback) => data.collection('posts').onSnapshot(callback);
 const deletePost = (id) => data.collection("posts").doc(id).delete();
 const editPost = (id, updatedPost) => data.collection('posts').doc(id).update(updatedPost);
 const getPostEditar = (id) => data.collection("posts").doc(id).get();
+let editStatus = false;
+let id = '';
 
 
 
-const createPlacePost = (e) => {//revisar
-  e.preventDefault();
-  
-  crearPost(titulo.value, descripcion.value);
-  const overLay = document.getElementById('overLay');
-  const popUp = document.getElementById('popUp');
-  overLay.classList.remove('active');
-  popUp.classList.remove('active');
-};
+
+// const createPlacePost = async(e) => {//revisar
+//   e.preventDefault();
+
+
+//   if(!editStatus){
+//     await crearPost(titulo.value, descripcion.value);
+
+//   }else{
+
+//   }
+//   const overLay = document.getElementById('overLay');
+//   const popUp = document.getElementById('popUp');
+//   overLay.classList.remove('active');
+//   popUp.classList.remove('active');
+// };
 
 
 
@@ -86,27 +95,34 @@ const postList = async () => {
 
       btn.addEventListener("click", async (event) => {
         try {
-
           const doc = await getPostEditar(event.target.dataset.id);
           const postsEdit = doc.data();
-          const overLay = document.getElementById('overLay');
+                    const overLay = document.getElementById('overLay');
           const popUp = document.getElementById('popUp');
           const btnCerrarPopup = document.getElementById('cerrarPopup');
 
+
+
           // activamos el pop up automaticamente con el click
-            overLay.classList.add('active');
-            popUp.classList.add('active');
+          overLay.classList.add('active');
+          popUp.classList.add('active');
+
+          editStatus = true;
+          id = doc.id;
+          formPublicacion['btnPublicar'].innerText = 'Actualizar';
 
           // Cerramos el pop up con la X
-            btnCerrarPopup.addEventListener('click', (e) => {
+          btnCerrarPopup.addEventListener('click', (e) => {
             e.preventDefault();
             overLay.classList.remove('active');
             popUp.classList.remove('active');
           });
-         
+
           formPublicacion['titulo'].value = postsEdit.titulo;
           formPublicacion['descripcion'].value = postsEdit.descripcion;
-         
+          
+
+
         }
         catch (error) {
           console.log(error);
@@ -154,8 +170,32 @@ export const mostrarMuro = async () => {
   appenMuro.style.display = 'flex';
 
   const formPublicacion = document.getElementById('formPublicacion');
-  formPublicacion.addEventListener('submit', createPlacePost);
-  formPublicacion.reset();
+  // formPublicacion.addEventListener('submit', createPlacePost);
+  // formPublicacion.reset();
+  formPublicacion.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const titulo = formPublicacion['titulo']
+    const descripcion = formPublicacion['descripcion'];
+    try {
+      if (!editStatus) {
+        await crearPost(titulo.value, descripcion.value);
+      } else {
+        await editPost(id, {
+          titulo: titulo.value,
+          descripcion: descripcion.value,
+        })
+        editStatus = false;
+        id = '';
+        formPublicacion['btnPublicar'].innerText= 'Publicar';
+      }
+      formPublicacion.reset();
+      titulo.focus();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
   await postList();
 
   // Popup Publicaciones
