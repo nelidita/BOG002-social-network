79c9f762c1cbad303931d1fdced84bab3c9d4ca8
Nelida Valero <nelidavalero06@gmail.com>
2021-06-03 18:38:32 -0500

    test

--- src/firebaseAuth.js
+++ src/firebaseAuth.js
@@ -9,42 +9,41 @@ export const registerUSer = (emailRegistro, passwordRegistro) => {
         text: 'Ya puedes disfrutar de nuestro contenido',
         icon: 'success'
       });
-      // swal('Bienvenido a Beer Lovers', userCredential.user);
-    })
-    .catch((error) => {
+
+    }).catch((error) => {
       swal({
         title: 'Este usuario ya existe.',
         text: 'Por favor intente con una cuenta válida',
         icon: 'error'
       });
 
-      // alert('Usuario ya existe. Por favor intente con una cuenta válida', error.message);
     });
+
 };
 
 export const loginUSer = (emailLogin, passwordLogin) => {
-  firebase
+  return firebase
     .auth()
     .signInWithEmailAndPassword(emailLogin, passwordLogin)
     .then((userCredential) => {
+
       window.location.hash = '#/posts'; // Con esto si el usuario se loguea correctamente muestra el muro.
 
-      swal({
-        title: 'Muy bien!!! Eres un  Beer Lovers.',
-        text: 'Bienvenido',
-        icon: 'success'
-      });
-      // swal('Muy bien!!! Eres un Beer Lovers. Bienvenido', userCredential.user);
+      // swal({
+      //   title: 'Muy bien!!! Eres un  Beer Lovers.',
+      //   text: 'Bienvenido',
+      //   icon: 'success'
+      // });
 
-    })
-    .catch((error) => {
-      swal({
-        title: 'Contraseña no válida.',
-        text: 'Por favor vuelve a intentarlo',
-        icon: 'error'
-      });
-      // swal('Contraseña no válida. Vuelve a intentarlo', error.message);
+    }).catch((error) => {
+      // swal({
+      //   title: 'Contraseña no válida.',
+      //   text: 'Por favor vuelve a intentarlo',
+      //   icon: 'error'
+      // });
+
     });
+
 };
 
 export const registroGmail = () => {
@@ -58,86 +57,24 @@ export const registroGmail = () => {
         text: 'Bienvenido a Beer Lover',
         icon: 'success'
       });
-      // window.location.hash = '#/posts';
-      // swal('Ingresaste correctamente. Bienvenido a Beer Lovers', result);
-    })
-    .catch((error) => {
+
+    }).catch((error) => {
       swal({
         title: 'Cuenta no válida.',
         text: 'Por favor use una cuenta válida',
         icon: 'error'
       });
-      // swal('Por favor usa una cuenta válida', error);
+
     });
+
 };
+
 export const cerrarSesion = () => {
-firebase.auth().signOut().then(() => {
-  window.location.hash = '#/Inicio';
-  //Sign-out successful.
-}).catch((error) => {
-  // An error happened.
-});
+  firebase.auth().signOut().then(() => {
+
+    window.location.hash = '#/Inicio';
+
+  }).catch((error) => {
+
+  });
 }
-
-// const usuarioActual = () => {
-//   firebase.auth().onAuthStateChanged((user)
-//     .then(() => {
-//       user.providerData.forEach((profile) => {
-//         console.log("Sign-in provider: " + profile.providerId);
-//         console.log("  Provider-specific UID: " + profile.uid);
-//         console.log("  Name: " + profile.displayName);
-//         console.log("  Email: " + profile.email);
-//         console.log("  Photo URL: " + profile.photoURL);
-//       });
-//     })
-//     .catch((error) => {
-
-//     })
-//   )
-//   };
-// usuarioActual();
-
-  // export const usuarioActual = () => {
-  //   firebase
-  //   .auth()
-  //   .onAuthStateChanged(user)
-  //   .then(() => {
-
-  //       user.providerData.forEach((profile) => {
-  //         console.log("Sign-in provider: " + profile.providerId);
-  //         console.log("  Provider-specific UID: " + profile.uid);
-  //         console.log("  Name: " + profile.displayName);
-  //         console.log("  Email: " + profile.email);
-  //         console.log("  Photo URL: " + profile.photoURL);
-  //       })
-  //       .catch((error) => {
-  //       console.log('Nadie logueado');
-  //       // No user is signed in.
-  //       })
-  //   })
-
-  // }
-
-  
-
-    
-
-    // const usuarioActual = () => {
-    //   firebase.auth().onAuthStateChanged((user) => {
-    //     console.log(user);
-    //     if (user != null) {
-    //       user.providerData.forEach((profile) => {
-    //         console.log("Sign-in provider: " + profile.providerId);
-    //         console.log("  Provider-specific UID: " + profile.uid);
-    //         console.log("  Name: " + profile.displayName);
-    //         console.log("  Email: " + profile.email);
-    //         console.log("  Photo URL: " + profile.photoURL);
-    //       });
-    //     } else {
-    //       console.log('Nadie logueado');
-    //       // No user is signed in.
-    //     }
-    //   });
-    //   }
-    //   usuarioActual();
-
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,68 @@
-// importamos la funcion que vamos a testear
-/* import { myFunction } from '../src/lib/index';
+import { loginUSer } from '../src/firebaseAuth.js';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+const firebasemock = require('firebase-mock');
+
+const mockauth = new firebasemock.MockAuthentication();
+// const mockfirestore = new firebasemock.MockFirestore();
+// const mockstorage = new firebasemock.MockStorage();
+const mocksdk = new firebasemock.MockFirebaseSdk(
+  () => {
+    return null;
+  },
+  // use null if your code does not use AUTHENTICATION
+  () => {
+    return mockauth;
+  }
+  // use null if your code does not use FIRESTORE
+  // () => {
+  //   return mockfirestore;
+  // },
+  // // use null if your code does not use STORAGE
+  // () => {
+  //   return mockstorage;
+  // },
+
+);
+
+mockauth.autoFlush();
+global.firebase = mocksdk;
+
+
+
+// describe('registerUSer', () => {
+//   test('debería ser una función', () => {
+//     expect(typeof registerUSer).toBe('function');
+//   });
+
+// });
+
+describe('loginUSer', () => {
+  test('debería ser una función', () => {
+    expect(typeof loginUSer).toBe('function');
   });
-}); */
+
+  test('deberia poder loguearme', () => {
+    const promesa = loginUSer('usuario@beerlovers.com', '12345678')
+    return promesa
+      .then((user) => {
+        expect(typeof user).toBe('object');
+        expect(user.email).toBe('usuario@beerlovers.com')
+      })
+
+  })
+
+});
+
+
+
+// describe('registroGmail', () => {
+//   test('debería ser una función', () => {
+//     expect(typeof registroGmail).toBe('function');
+//   });
+// });
+
+// describe('cerrarSesion', () => {
+//   test('debería ser una función', () => {
+//     expect(typeof cerrarSesion).toBe('function');
+//   });
+// });
